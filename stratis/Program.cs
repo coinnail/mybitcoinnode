f991c86c6a1e88b154d9560e98206cb29016426d
coinnail <coinnail@users.noreply.github.com>
2017-12-26 21:20:25 +0000

    added stratis

---
+++ stratis/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace stratis
+{
+    using Stratis.Bitcoin;
+    using Stratis.Bitcoin.Builder;
+    using Stratis.Bitcoin.Configuration;
+    using Stratis.Bitcoin.Features.BlockStore;
+    using Stratis.Bitcoin.Features.Consensus;
+    using Stratis.Bitcoin.Features.MemoryPool;
+    using Stratis.Bitcoin.Features.Miner;
+    using Stratis.Bitcoin.Features.RPC;
+    using Stratis.Bitcoin.Features.Wallet;
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NodeSettings nodeSettings;
+            IFullNodeBuilder fullNodeBuilder;
+            IFullNode fullNode;
+
+            try
+            {
+                nodeSettings = new NodeSettings().LoadArguments(args);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("There was a problem initializing node settings. Details: '{0}'", exception.Message);
+                Console.ReadLine();
+                return;
+            }
+
+            try
+            {
+                fullNodeBuilder = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseConsensus()
+                    .UseBlockStore()
+                    .UseMempool()
+                    .AddMining()
+                    .AddRPC()
+                    .UseWallet();
+
+                fullNode = fullNodeBuilder.Build();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", exception.Message);
+                Console.ReadLine();
+                return;
+            }
+
+            try
+            {
+                fullNode.Start();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("There was a problem starting the node. Details: '{0}'", exception.Message);
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
